b71a9cdcee3be747d08f54ad786aae6a8c25503b
pablomm <pablomm@users.noreply.github.com>
2024-08-12 17:00:06 +0200

    Add project information

---
+++ README.md
@@ -0,0 +1,1 @@
+# Brainfuck Art
--- brainfuck_art/__init__.py
+++ brainfuck_art/__init__.py
@@ -4,8 +4,10 @@ from .image_matrix_generator import image_to_matrix
 from .output_html import save_matrix_to_html
 from .output_svg import save_matrix_to_svg
 from .output_pdf import save_matrix_to_pdf
+from .__version__ import __version__
 
 __all__ = [
+    "__version__",
     "text_to_bf",
     "execute_bf",
     "image_to_matrix",
